3684c6cba9bcf4a1ec157ba5efcab275cf3a2a7c
cangjie <762741539@qq.com>
2023-09-19 09:05:49 +0800

    项目初始化

---
+++ src/test/java/com/lizhenggen/traindemo/TrainDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lizhenggen.traindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
